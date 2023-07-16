f45d505171e6b1da8f2eb661776e8bb31e11dd47
Tim Kreitner <krtim@kreitner.xyz>
2023-07-16 22:04:31 +0200

    setup db

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,5 +1,7 @@
 use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
+use shuttle_runtime::CustomError;
+use sqlx::Executor;
 
 #[get("/")]
 async fn hello_world() -> &'static str {
@@ -13,6 +15,11 @@ async fn actix_web(
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
     };
+
+    // initialize the database if not already initialized
+    pool.execute(include_str!("../../db/schema.sql"))
+    .await
+    .map_err(CustomError::new)?;
 
     Ok(config.into())
 }
